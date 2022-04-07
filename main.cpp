45f92565625afcd56fc91e358ba488711f6affa5
expressxen <97218884+expressionLux@users.noreply.github.com>
2022-04-07 21:18:50 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 #include <windows.h>
+// function that we will "jump" to
 void print_woo() {
     std::cout << "woo" << "\n";
 }
 
 // veh function. this is the function that will allow us to "jump" to our print_woo function.
 
+
 LONG WINAPI veh(EXCEPTION_POINTERS *exception_info) {
     // check if we get a page violation.
     if(exception_info->ExceptionRecord->ExceptionCode == STATUS_GUARD_PAGE_VIOLATION) {
         // check if we're at the right address for the function we want to hook
-        if(exception_info->ContextRecord->Eip == (uintptr_t)printf) {
+        if(exception_info->ContextRecord->Eip == reinterpret_cast<uintptr_t>(printf)) {
             // change the instruction pointer to point to our function.
-            exception_info->ContextRecord->Eip = (uintptr_t)print_woo;
+            exception_info->ContextRecord->Eip = reinterpret_cast<uintptr_t>(print_woo);
         }
         // enable single stepping mode
         exception_info->ContextRecord->EFlags |= 0x100;
@@ -23,7 +25,7 @@ LONG WINAPI veh(EXCEPTION_POINTERS *exception_info) {
     // after "stepping"
     if(exception_info->ExceptionRecord->ExceptionCode == STATUS_SINGLE_STEP) {
         DWORD prot_buff;
-        VirtualProtect((LPVOID)printf, 1, PAGE_EXECUTE_READ | PAGE_GUARD, &prot_buff);
+        VirtualProtect(reinterpret_cast<PVOID>(printf), 1, PAGE_EXECUTE_READ | PAGE_GUARD, &prot_buff);
         return EXCEPTION_CONTINUE_EXECUTION;
     }
     return EXCEPTION_CONTINUE_SEARCH;
@@ -36,8 +38,8 @@ int main() {
     DWORD prot_buff;
 
     // check that the virtual queries we make go properly.
-    if(!VirtualQuery((const uintptr_t*)printf, &printf_info, sizeof(printf_info))) { printf("Failed to query basic information with VirtualQuery.\nGLE(get last error) code: %i", GetLastError()); return 0; }
-    if(!VirtualQuery((const uintptr_t*)print_woo, &printwoo_info, sizeof(printwoo_info))) { printf("%s", "Failed to query basic information with VirtualQuery.\nGLE(get last error) code: %i", GetLastError()); return 0;}
+    if(!VirtualQuery(reinterpret_cast<const uintptr_t*>(printf), &printf_info, sizeof(printf_info))) { printf("Failed to query basic information with VirtualQuery.\nGLE(get last error) code: %i", GetLastError()); return 0; }
+    if(!VirtualQuery(reinterpret_cast<const uintptr_t*>(print_woo), &printwoo_info, sizeof(printwoo_info))) { printf("%s", "Failed to query basic information with VirtualQuery.\nGLE(get last error) code: %i", GetLastError()); return 0;}
     
     // check if our function is in the same page as printf. if it is we cannot hook it.
     if(printf_info.BaseAddress == printwoo_info.BaseAddress) {
@@ -46,12 +48,12 @@ int main() {
     }
 
     // add our veh.
-    veh_handle = AddVectoredExceptionHandler(1, (PVECTORED_EXCEPTION_HANDLER)veh);
+    veh_handle = AddVectoredExceptionHandler(1, reinterpret_cast<PVECTORED_EXCEPTION_HANDLER>(veh));
     
     // check if adding our exception handler went well, if it did then enable the pageguard flag in the mem page
     // printf is in.
     if(veh_handle == NULL) { printf("%s", "Failed to add vectored exception handler."); }
-    if(!VirtualProtect((LPVOID)printf, 1, PAGE_EXECUTE_READ | PAGE_GUARD, &prot_buff)) { printf("Failed to enable page guard with VirtualProtect.\nGLE(get last error) code: %i", GetLastError()); return 0;}
+    if(!VirtualProtect(reinterpret_cast<PVOID>(printf), 1, PAGE_EXECUTE_READ | PAGE_GUARD, &prot_buff)) { printf("Failed to enable page guard with VirtualProtect.\nGLE(get last error) code: %i", GetLastError()); return 0;}
     
     printf("%s", "Hooked.");
     printf("%s", "Hooked x2.");
